864be2ccd1d94c65683b1036b4f11c7a5b3f1449
Raúl Vázquez Ramos <Rawvr@hotmail.com>
2018-02-15 20:59:10 +0100

    Submenús y cambios menores
    Cambios a program.cs
    -------------------------
    Añadidos los menús:
    - Aulas.
    - Ordenadores.
    - Búsquedas.
    - Listados.
    - Configuración.
    - También habilitada la opción para salir del programa poniendo 0.
    
    Las opciones del menú búsquedas deberán ser a imaginación de los alumnos. (Pensar en opciones una vez acabe los demás apartados).
    
    Añadidas las líneas al menú principal para introducir a que submenú queremos ir.

--- Problema_1/Program.cs
+++ Problema_1/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 Autor: Raúl Vázquez Ramos
 Fecha creación:      13/02/2018
 Última modificación: 13/02/2018
-Versión: 0.01.2
+Versión: 0.04.3
 ***********************************/
 
 namespace Problema_1
@@ -16,6 +16,7 @@ namespace Problema_1
     class Program
     {
         static int posX = 0, posY = 0;
+        static string nMenu = string.Empty;
 
         static void Main(string[] args)
         {
@@ -24,20 +25,138 @@ namespace Problema_1
 
         static void Menu()
         {
-            string nMenu = string.Empty;
+            Console.SetWindowSize(65, 20);
             do
             {
-                WriteFrameAt(10, 2, 70, 11, true);
-                WriteAt("=== GESTIÓN DE ORDENADORES ===", 27, 3);
+                WriteFrameAt(10, 2, 50, 11, true);
+                WriteAt("=== GESTIÓN DE ORDENADORES ===", 16, 3);
                 WriteAt("1 - Aulas/Salas del centro.", 12, 5);
                 WriteAt("2 - Ordenadores.", 12, 6);
                 WriteAt("3 - Búsquedas.", 12, 7);
                 WriteAt("4 - Listados.", 12, 8);
                 WriteAt("5 - Configuración.", 12, 9);
-                WriteAt("-----------------------------", 12, 10);
+                WriteAt("----------------------------", 12, 10);
                 WriteAt("0 - Salir.", 12, 11);
-                WriteAt("Ir a:", 12, 14);
+                WriteAt("Ir a: ", 12, 14);
+                nMenu = Console.ReadLine();
+                Console.Clear();
             } while (nMenu != "0" && nMenu != "1" && nMenu != "2" && nMenu != "3" && nMenu != "4" && nMenu != "5");
+            switch (nMenu)
+            {
+                case "1":
+                    MenuAula();
+                    break;
+                case "2":
+                    MenuOrdenadores();
+                    break;
+                case "3":
+                    MenuBusquedas();
+                    break;
+                case "4":
+                    MenuListados();
+                    break;
+                case "5":
+                    MenuConfiguracion();
+                    break;
+                default:
+                    Exit();
+                    break;
+            }
+        }
+
+        static void MenuAula()
+        {
+            Console.SetWindowSize(65, 20);
+            do
+            {
+                WriteFrameAt(10, 2, 50, 10, true);
+                WriteAt("=== AULAS ===", 24, 3);
+                WriteAt("1 - Ver aulas.", 12, 5);
+                WriteAt("2 - Añadir aula.", 12, 6);
+                WriteAt("3 - Borrar aula.", 12, 7);
+                WriteAt("4 - Modificar aula.", 12, 8);
+                WriteAt("------------------------------", 12, 9);
+                WriteAt("0 - Volver al menú principal.", 12, 10);
+                WriteAt("Ir a: ", 12, 13);
+                nMenu = Console.ReadLine();
+                Console.Clear();
+            } while (nMenu != "0" && nMenu != "1" && nMenu != "2" && nMenu != "3" && nMenu != "4");
+            switch (nMenu)
+            {
+                case "1":
+                    //();
+                    break;
+                case "2":
+                    //();
+                    break;
+                case "3":
+                    //();
+                    break;
+                case "4":
+                    //();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void MenuOrdenadores()
+        {
+            Console.SetWindowSize(65, 20);
+            do
+            {
+                WriteFrameAt(10, 2, 50, 11, true);
+                WriteAt("=== ORDENADORES ===", 20, 3);
+                WriteAt("1 - Ver lista de ordenadores.", 12, 5);
+                WriteAt("2 - Añadir ordenador.", 12, 6);
+                WriteAt("3 - Borrar ordenador.", 12, 7);
+                WriteAt("4 - Cambiar ordenador de aula.", 12, 8);
+                WriteAt("5 - Modificar ordenador.", 12, 9);
+                WriteAt("-------------------------------", 12, 10);
+                WriteAt("0 - Volver al menú principal.", 12, 11);
+                WriteAt("Ir a: ", 12, 14);
+                nMenu = Console.ReadLine();
+                Console.Clear();
+            } while (nMenu != "0" && nMenu != "1" && nMenu != "2" && nMenu != "3" && nMenu != "4" && nMenu != "5");
+            switch (nMenu)
+            {
+                case "1":
+                    MenuAula();
+                    break;
+                case "2":
+                    //();
+                    break;
+                case "3":
+                    //();
+                    break;
+                case "4":
+                    //();
+                    break;
+                case "5":
+                    //();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void MenuBusquedas()
+        {
+            // Apartado avanzado donde se implementarán búsquedas según mi imaginación.
+            Console.SetWindowSize(65, 20);
+            do
+            {
+                WriteFrameAt(10, 2, 50, 7, true);
+                WriteAt("=== BÚSQUEDAS ===", 22, 3);
+                WriteAt("En construcción...", 12, 5);
+                WriteAt("-------------------------------", 12, 6);
+                WriteAt("0 - Volver al menú principal.", 12, 7);
+                WriteAt("Ir a: ", 12, 10);
+                nMenu = Console.ReadLine();
+                Console.Clear();
+            } while (nMenu != "0");
             switch (nMenu)
             {
                 case "1":
@@ -56,7 +175,77 @@ namespace Problema_1
                     //();
                     break;
                 default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void MenuListados()
+        {
+            Console.SetWindowSize(92, 20);
+            do
+            {
+                WriteFrameAt(10, 2, 80, 10, true);
+                WriteAt("=== Listados ===", 35, 3);
+                WriteAt("1 - Nº de ordenadores por aula.", 12, 5);
+                WriteAt("2 - Lista de ordenadores ordenados por aula e identificador.", 12, 6);
+                WriteAt("3 - Aplicaciones instaladas por cada ordenador.", 12, 7);
+                WriteAt("4 - Características de cada ordenador.", 12, 8);
+                WriteAt("-------------------------------------------------------------", 12, 9);
+                WriteAt("0 - Volver al menú principal.", 12, 10);
+                WriteAt("Ir a: ", 12, 13);
+                nMenu = Console.ReadLine();
+                Console.Clear();
+            } while (nMenu != "0" && nMenu != "1" && nMenu != "2" && nMenu != "3" && nMenu != "4");
+            switch (nMenu)
+            {
+                case "1":
                     //();
+                    break;
+                case "2":
+                    //();
+                    break;
+                case "3":
+                    //();
+                    break;
+                case "4":
+                    //();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void MenuConfiguracion()
+        {
+            Console.SetWindowSize(75, 20);
+            do
+            {
+                WriteFrameAt(10, 2, 60, 9, true);
+                WriteAt("=== Listados ===", 27, 3);
+                WriteAt("1 - Número máximo de aulas.", 12, 5);
+                WriteAt("2 - Número máximo de ordenadores por aula.", 12, 6);
+                WriteAt("3 - Inicialización automática para pruebas.", 12, 7);
+                WriteAt("--------------------------------------------", 12, 8);
+                WriteAt("0 - Volver al menú principal.", 12, 9);
+                WriteAt("Ir a: ", 12, 12);
+                nMenu = Console.ReadLine();
+                Console.Clear();
+            } while (nMenu != "0" && nMenu != "1" && nMenu != "2" && nMenu != "3");
+            switch (nMenu)
+            {
+                case "1":
+                    //();
+                    break;
+                case "2":
+                    //();
+                    break;
+                case "3":
+                    //();
+                    break;
+                default:
+                    Menu();
                     break;
             }
         }
@@ -147,6 +336,11 @@ namespace Problema_1
                 }
             } while (Num < 0 || Num >= 0);
             return Num;
+        }
+
+        static void Exit()
+        {
+            WriteAt("Adioooooooooos", 12, 3);
         }
     }
 }
